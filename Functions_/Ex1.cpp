c33310038d273851c3dcd52d8177ab561ee12f79
Christian <christian@chrischrisss-5.local>
2026-01-16 21:44:10 -0800

    changes

--- Functions_/Ex1.cpp
+++ Functions_/Ex1.cpp
@@ -42,6 +42,8 @@ int main() {
     std::cout << area << std::endl;
     std::cout << square_area << std::endl;
     std::cout << namespace_triangle_area << std::endl;
+    std::cout << example::cirlce(2) << std::endl;
+    
     return 0;
 }
 
